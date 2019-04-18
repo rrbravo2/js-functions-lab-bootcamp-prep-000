ed86c36443fbbb1f7403a66a8293c04c4730bd0f
Ryan Bravo <rrbravo2@gmail.com>
2019-04-18 17:56:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
 
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
